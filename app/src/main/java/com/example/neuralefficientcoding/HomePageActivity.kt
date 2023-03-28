03163303c21ba72d6d4a8c96250be882bdc72948
harikiran <harikirankeerthipati@gmail.com>
2023-03-28 13:22:18 -0500

    Refactor HomePageActivity.kt.

--- app/src/main/java/com/example/neuralefficientcoding/HomePageActivity.kt
+++ app/src/main/java/com/example/neuralefficientcoding/HomePageActivity.kt
@@ -1,7 +1,6 @@
 package com.example.neuralefficientcoding
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -10,12 +9,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.neuralefficientcoding.about.AboutPage1Activity
 import com.example.neuralefficientcoding.about.AppInfoActivity
 import com.example.neuralefficientcoding.databinding.ActivityHomepageBinding
-import com.example.neuralefficientcoding.databinding.ActivityWelcomeBinding
 
 class HomePageActivity : AppCompatActivity() {
     private lateinit var homePageBinding: ActivityHomepageBinding
-    private lateinit var welcomePageBinding: ActivityWelcomeBinding
-    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,29 +25,8 @@ class HomePageActivity : AppCompatActivity() {
 
         }
 
-        preferences = getSharedPreferences("com.example.neuroscience", MODE_PRIVATE)
-
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        if (preferences.getBoolean("first_run", true)) {
-            welcomePageBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
-            supportActionBar?.hide()
-
-            welcomePageBinding.exitText.setOnClickListener {
-                startActivity(Intent(this, HomePageActivity::class.java))
-                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
-            }
-            welcomePageBinding.nextText.setOnClickListener {
-                startActivity(Intent(this, AboutPage1Activity::class.java))
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-            }
-
-            preferences.edit().putBoolean("first_run", false).apply()
-        }
-    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.info_menu, menu)
