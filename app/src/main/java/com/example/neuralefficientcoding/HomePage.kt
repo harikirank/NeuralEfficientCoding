967dd8c91d73a5fd959bf731fbb636f6af763918
harikiran <harikirankeerthipati@gmail.com>
2023-03-27 22:48:08 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/neuralefficientcoding/HomePage.kt
@@ -0,0 +1,11 @@
+package com.example.neuralefficientcoding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
